543df831afd52fc6a67a7ed9d3ae503b81a67664
ChronoLogic <msuttie12@hotmail.com>
2022-02-01 21:06:54 +0000

    Create funtionality to add todo list items to the database

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
 
 # Create your views here.
@@ -12,3 +12,11 @@ def get_todo_list(request):
     }
     return render(request, "todo/todo_list.html", context)
     
+
+def add_item(request):
+    if request.method == "POST":
+        name = request.POST.get("item_name")
+        done = "done" in request.POST
+        Item.objects.create(name=name, done=done)
+        return redirect("get_todo_list")
+    return render(request, "todo/add_item.html")
